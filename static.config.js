b8a717a3e638a10c928a5d0588dd2f67b0f793a2
David York <davideyork@gmail.com>
2018-01-05 06:50:45 -0700

    Favicon madness

--- static.config.js
+++ static.config.js
@@ -170,6 +170,11 @@ export default {
             <title>David York</title>
             <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.11/semantic.min.css" />
             <link rel="stylesheet" href="/css/davideyork.css" />
+            <link rel="shortcut icon" href="/favicon.ico" />
+            <link rel="apple-touch-icon" href="/content/images/apple-touch-icon.png" />
+            <link rel="apple-touch-icon" sizes="72x72" href="/content/images/apple-touch-icon-72x72.png" />
+            <link rel="apple-touch-icon" sizes="114x114" href="/content/images/apple-touch-icon-114x114.png" />
+            <link rel="apple-touch-icon" sizes="144x144" href="/content/images/apple-touch-icon-144x144.png" />
             { renderMeta.styleTags }
           </Head>
           <Body>{ children }</Body>
